93aff974d4648733e9fc130ee39e0869911b2f5e
HafizArham71 <hafizarhammujahid71@gmail.com>
2025-10-05 11:08:31 +0500

    New Object Created

--- JAVA/Projects/02_StudentGradeTracker/Demo.java
+++ JAVA/Projects/02_StudentGradeTracker/Demo.java
@@ -7,5 +7,8 @@ public class Demo{
 
 		Student s2 = new Student(17, "Arham Mujahid", 4);
 		System.out.println(s2);
+		
+		Student s3 = new Student(24, "Ahmad Iftikhar", 6);
+		System.out.println(s3);
 	}
 }
