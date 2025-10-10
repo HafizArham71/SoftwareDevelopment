f71956a6e90a23c6ae6e727037c323229f44e8b0
HafizArham71 <hafizarhammujahid71@gmail.com>
2025-10-10 06:35:40 +0500

    AssignmentStatement Added

---
+++ JAVA/Course/01_Lecture/01_BasicProgram/Demo.java
@@ -0,0 +1,21 @@
+public class Demo{
+
+	public static void main(String[] args){
+		
+		// This is my first Java Program
+
+		// It will take a whole line
+		System.out.println("Hello Hafiz Arham!");
+
+		// It will take inline space
+		System.out.print("Hello ");
+		System.out.print("Hafiz ");
+		System.out.print("Arham ");
+
+		/*
+		This is 
+		a multi-line
+		comment.
+		*/
+	}	
+}
